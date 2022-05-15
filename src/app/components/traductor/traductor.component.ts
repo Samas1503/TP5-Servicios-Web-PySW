2745f8e02909ba4fdda7a2065dde442cd31056dc
¨Samas1503¨ <samueleliasparedes.10@gmail.com>
2022-05-15 12:20:26 -0300

    Trabajando con peliculas

--- src/app/components/traductor/traductor.component.ts
+++ src/app/components/traductor/traductor.component.ts
@@ -5,16 +5,17 @@ import { async } from '@angular/core/testing';
 @Component({
   selector: 'app-traductor',
   templateUrl: './traductor.component.html',
-  styleUrls: ['./traductor.component.css']
+  styleUrls: ['./traductor.component.css'],
 })
 export class TraductorComponent implements OnInit {
-
   textoTraducido!: string;
   texto!: string;
   source!: string;
   target!: string;
 
   constructor(private traductor: TranstaleService) {
+    this.source = 'it';
+    this.target = 'it';
   }
 
   ngOnInit(): void {
@@ -32,14 +33,23 @@ export class TraductorComponent implements OnInit {
     this.texto = texto;
   }
 
-  async traducir(source: string, target:string, texto:string) {
-    (await this.traductor.translateText(source, target, texto)).subscribe(async (res) => {
-      this.textoTraducido = await res.data.translations.translatedText;
-    });
+  async traducir(source: string, target: string, texto: string) {
+    (await this.traductor.translateText(source, target, texto)).subscribe(
+      async (res) => {
+        this.textoTraducido = await res.data.translations.translatedText;
+      }
+    );
   }
+
+  // async detectar(texto: string) {
+  //   (await this.traductor.detectLanguage(texto)).subscribe(
+  //     async (res) => {
+  //       await console.log(res.data.detections[0].languaje);
+  //     }
+  //   );
+  // }
 
   copiar(texto: string) {
     navigator.clipboard.writeText(texto);
   }
-
 }
--- src/app/services/films.service.ts
+++ src/app/services/films.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FilmsService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  public getMovie(id: string): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'X-RapidAPI-Host': 'movie-details1.p.rapidapi.com',
+        'X-RapidAPI-Key': 'f5bd839dc5msh43cdd353da82d74p17443cjsna0c6fb0266df',
+      }),
+    };
+    return this.http.get(
+      'https://movie-details1.p.rapidapi.com/imdb_api/movie/?id='+id,
+      httpOptions
+    );
+  }
 }
